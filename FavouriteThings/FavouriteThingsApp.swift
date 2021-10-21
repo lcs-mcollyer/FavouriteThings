11e18d7f34576bc78f35bc58b25088180fba4729
matt collyer <mcollyer@lcs.on.ca>
2021-10-21 14:54:40 -0400

    Initial Commit

---
+++ FavouriteThings/FavouriteThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavouriteThingsApp.swift
+//  FavouriteThings
+//
+//  Created by Matthew Collyer on 2021-10-21.
+//
+
+import SwiftUI
+
+@main
+struct FavouriteThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
